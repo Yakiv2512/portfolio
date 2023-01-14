a0dc367630c9058b3bffc8cb1f07d2f4de7f8b32
Vlad <yakiv2512@gmail.com>
2023-01-14 15:35:37 +0100

    finished desctop version

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,13 @@
 const   hamburger   = document.querySelector('.hamburger'),
         menu        = document.querySelector('.menu'),
         container       = document.querySelector('.container'),
-        close       = document.querySelector('.menu__close');
+        close       = document.querySelector('.menu__close'),
+        item1 = document.querySelector('.item1'),
+        item2 = document.querySelector('.item2'),
+        item3 = document.querySelector('.item3'),
+        item4 = document.querySelector('.item4'),
+        item5 = document.querySelector('.item5'),
+        item6 = document.querySelector('.item6');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -12,6 +18,37 @@ close.addEventListener('click', () => {
     menu.classList.remove('active');
     container.classList.remove('active');
 });
+
+item1.addEventListener('click', () => {
+    menu.classList.remove('active');
+    container.classList.remove('active');
+});
+
+item2.addEventListener('click', () => {
+    menu.classList.remove('active');
+    container.classList.remove('active');
+});
+
+item3.addEventListener('click', () => {
+    menu.classList.remove('active');
+    container.classList.remove('active');
+});
+
+item4.addEventListener('click', () => {
+    menu.classList.remove('active');
+    container.classList.remove('active');
+});
+
+item5.addEventListener('click', () => {
+    menu.classList.remove('active');
+    container.classList.remove('active');
+});
+
+item6.addEventListener('click', () => {
+    menu.classList.remove('active');
+    container.classList.remove('active');
+});
+
 
 // script для изменения %
 
